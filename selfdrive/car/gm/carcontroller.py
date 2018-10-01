eada643a60484e44ca889801b71701802994e3cc
Jamezz <di.jamezz@gmail.com>
2018-09-30 18:15:43 -0700

    Try more restrictive torque limits for acadia

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -18,8 +18,8 @@ class CarControllerParams():
     elif car_fingerprint == CAR.ACADIA_DENALI:
       self.STEER_MAX = 300
       self.STEER_STEP = 2              # how often we update the steer cmd
-      self.STEER_DELTA_UP = 7          # ~0.75s time to peak torque (255/50hz/0.75s)
-      self.STEER_DELTA_DOWN = 17       # ~0.3s from peak torque to zero
+      self.STEER_DELTA_UP = 5          # ~0.75s time to peak torque (255/50hz/0.75s)
+      self.STEER_DELTA_DOWN = 14       # ~0.3s from peak torque to zero
     elif car_fingerprint == CAR.CADILLAC_CT6:
       self.STEER_MAX = 150
       self.STEER_STEP = 1              # how often we update the steer cmd
